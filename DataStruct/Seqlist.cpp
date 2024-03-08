3363f33025d53631277fedaa72dd43ed7ff27b37
qza36 <an9qi@yandex.com>
2024-03-08 09:20:37 +0800

    添加项目文件。

---
+++ DataStruct/Seqlist.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+const int MaxSize = 10; //定义最大长度
+
+/*类模板 */
+
+template <class T> //类模板的定义
+class SeqList
+{
+private:
+	int length; //顺序表的长度
+	T data[MaxSize];
+public:
+	SeqList(){length = 0;} //构造函数
+	SeqList(T a[], int n);
+	~SeqList(){} //析构函数
+	//按位插入
+	//按值插入
+	void Print(); //输出顺序表
+
+};
+template <class T>
+SeqList<T>::SeqList(T a[], int n)
+{
+	length = n;
+	for(int k =0 ; k < length; k++)
+		data[k] = a[k];
+}
+
+template <class T>
+void SeqList<T>::Print() 
+{
+	cout << "顺序表的元素为：";
+	for(int k = 0; k < length; k++)
+		cout << data[k] << " ";
+	cout << endl;
+} //输出顺序表
+
+int main() {
+	int a[5] = {1,2,3,4,5};
+	SeqList<int> s1(a,5);
+	s1.Print();
+	return 0;
+}
