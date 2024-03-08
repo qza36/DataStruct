8bbfc46e2821b84c2360848be75f9d141142e486
qza36 <an9qi@yandex.com>
2024-03-08 14:43:24 +0800

    添加了按值插入和删除

--- DataStruct/Seqlist.cpp
+++ DataStruct/Seqlist.cpp
@@ -19,6 +19,14 @@ public:
 	//按位插入
 	void Insert(int locate,T x);
 	//按值插入
+	void Insert(T x);
+	//删除（带条件）
+	//查找（按值查找）
+	void FindX(T x);
+	//按位查找
+	void Find(int l);
+	//删除
+	void Delete(int l);
 	void Print(); //输出顺序表
 
 };
@@ -34,11 +42,59 @@ template <class T>
 void SeqList<T>::Insert(int locate, T x)
 {
 	//从最后一个元素开始到第locate个元素依次向后移动
-	for(knt k = length; k > locate; k--)
+	for(int k = length; k > locate; k--)
 		data[k] = data[k-1];
 	data[locate] = x;
 	length++;
 } //插入元素
+
+template<class T>
+void SeqList<T>::Insert(T x)
+{
+	if (length == MaxSize)
+	{
+		cout << "上溢" << endl;
+		return;
+	}
+	data[length] = x;
+	length++;
+} //插入元素
+
+template<class T>
+void SeqList<T>::FindX(T x)
+{
+	int k = 0;
+	while(k < length && data[k] != x)
+		k++;
+	if(k == length)
+		cout << "没有找到" << endl;
+	else
+		cout << "找到了" << endl;
+} //查找元素
+
+template<class T>
+void SeqList<T>::Find(int locate)
+{
+	if (locate < 0 || locate >= length)
+	{
+		cout << "位置不合法" << endl;
+		return;
+	}
+	cout << "第" << locate << "个元素为：" << data[locate] << endl;
+} //查找元素
+
+template<class T>
+void SeqList<T>::Delete(int locate)
+{
+	if (locate < 0 || locate >= length)
+	{
+		cout << "位置不合法" << endl;
+		return;
+	}
+	for(int k = locate; k < length-1; k++)
+		data[k] = data[k+1];
+	length--;
+} //删除元素
 
 template <class T>
 void SeqList<T>::Print() 
@@ -50,8 +106,29 @@ void SeqList<T>::Print()
 } //输出顺序表
 
 int main() {
-	int a[5] = {1,2,3,4,5};
-	SeqList<int> s1(a,5);
+	int x=1;
+	int i = 0;
+	int a[6] = {0};
+	while (x != 0) {
+		cin >> x;
+		a[i] = x;
+		i++;
+	}
+	SeqList<int> s1(a,6);
+	cout << "当前数组" ;
+	s1.Print();
+	cout << "输入想要插入的位置和数值" << endl;
+	int locate1;
+	int x1;
+	cin >> locate1 >> x1;
+	s1.Insert(locate1,x1);
+	cout << "插入后的数组" ;
+	s1.Print();
+	cout << "输入想要删除的位置"<<endl;
+	int locate;
+	cin >> locate;
+	s1.Delete(locate);
+	cout << "操作后的数组" ;
 	s1.Print();
 	return 0;
 }
